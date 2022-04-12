559d8509e51952fcde0bd09ba92bec674f7973ff
rjakkinappali-ontic <rjakkinappali@ontic.co>
2022-04-12 15:57:41 +0530

    commit

--- src/main/java/com/example/topicPartition/service/KafkaConsumerService.java
+++ src/main/java/com/example/topicPartition/service/KafkaConsumerService.java
@@ -2,15 +2,25 @@ package com.example.topicPartition.service;
 
 import com.example.topicPartition.constants.ConstantValues;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumerService {
-    @KafkaListener(topics = ConstantValues.topicNameString, groupId = ConstantValues.groupId)
+    @KafkaListener(groupId = ConstantValues.groupId, topicPartitions = @TopicPartition(
+            topic = ConstantValues.topicNameString,
+            partitionOffsets = { @PartitionOffset(
+                    partition = "0",
+                    initialOffset = "0") }))
     public void listenerString(String data){
         System.out.println("Received partition 0:  "+ data);
     }
-    @KafkaListener(topics = ConstantValues.topicNameString, groupId = ConstantValues.groupId)
+    @KafkaListener(groupId = ConstantValues.groupId, topicPartitions = @TopicPartition(
+            topic = ConstantValues.topicNameString,
+            partitionOffsets = { @PartitionOffset(
+                    partition = "1",
+                    initialOffset = "0") }))
     public void listenerString2(String data){
         System.out.println("Received partition 1:  "+ data);
     }
